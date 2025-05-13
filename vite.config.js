7173aa1c3cbfbf9010e9b1d53ad15714d867e904
Jai-jayapalan <jaijayathilak@gmail.com>
2025-05-13 12:50:40 +0530

    changed dist loc

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   base: '/',
   build: {
-    outDir: '/dist',
+    outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
     rollupOptions: {
